01726af97b001c9bab8b335a4caf7de96d9fb188
Tolotra <raltolotra@gmail.com>
2024-02-13 18:42:13 +0300

    function to push notification
    push notification for all device registred

--- app/firebase/firebaseUtils.js
+++ app/firebase/firebaseUtils.js
@@ -9,3 +9,27 @@ var Device = db.device;
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
+// Construct a notification message
+module.exports = async ( title, body) => {
+    const devicePushToken = await Device.find({});
+    for(let i = 0; i< devicePushToken.length; i++){
+        jwt.verify(devicePushToken[i].auth_token, SECRET_KEY, async(err, decoded) => {
+            if (err) {
+              console.error('Erreur de vérification du JWT :', err);
+            } else {
+                try{
+                    await admin.messaging().send({
+                            token: devicePushToken[i].token_registration,
+                            notification: {
+                                title,
+                                body 
+                            }
+                        })
+                    }
+                catch(error){
+                }
+            }
+          });
+    }
+}
