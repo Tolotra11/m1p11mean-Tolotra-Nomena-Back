a72f80d4b7ba7b2cba1adf27e2ec181e11d88b03
Tolotra <raltolotra@gmail.com>
2024-02-27 22:27:12 +0300

    Update manager.routes.js

--- app/routes/manager.routes.js
+++ app/routes/manager.routes.js
@@ -3,11 +3,17 @@ module.exports = app => {
     const userController = require('../controllers/user.controller');
     const tokenMiddleware = require('../middleware/token.middleware');
     const identifierMiddleware = require('../middleware/identifier.middleware'); 
+    const serviceController = require('../controllers/service.controller'); 
 
     router.post('/users',userController.registerEmploye);
     router.get("/users",userController.getUsers);
     router.get("/users/:id",userController.getUserById);
     router.delete('/users/:id',userController.deleteUser);
     router.put('/users/:id',userController.updateUser);
+
+    router.post('/services',serviceController.createService);
+    router.put('/services/:id',serviceController.updateService);
+    router.put('/services/:id',serviceController.deleteService);
+
     app.use('/api/managers', tokenMiddleware.checkTokenExistance,tokenMiddleware.decryptToken,identifierMiddleware.getInfoManager,router);
 }
