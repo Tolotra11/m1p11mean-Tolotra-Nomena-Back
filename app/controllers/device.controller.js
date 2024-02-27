162fff2f7b619ceee5ee5f72f2045a84629447b2
Tolotra <raltolotra@gmail.com>
2024-02-27 23:40:18 +0300

    Update device.controller.js

--- app/controllers/device.controller.js
+++ app/controllers/device.controller.js
@@ -1,9 +1,10 @@
+const { ERROR_STATUS_CODE } = require("../constant/Error.constant");
 const db = require("../models");
 const Device = db.device;
 exports.create = async(req,res) => {
     const auth_token = req.token;
     if(!req.body.token_registration || !req.decoded.userId || !auth_token){
-        res.status(400).send({ message: "Content can not be empty!" });
+        res.status(ERROR_STATUS_CODE.BAD_REQUEST).send({ message: "Content can not be empty!" });
         return;
     }
     try{
@@ -11,7 +12,7 @@ exports.create = async(req,res) => {
         res.send(device);
     }
     catch(error){
-        res.status(500).send({message:error.message})
+        res.status(ERROR_STATUS_CODE.INTERNAL_SERVER_ERROR).send({message:error.message})
     }
     
 }
