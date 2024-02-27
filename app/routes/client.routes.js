cb6e5ba873897db3e0880c9354af3ce76b32da18
Tolotra <marcnomena@gmail.com>
2024-02-27 23:11:31 +0300

    Update client.routes.js

--- app/routes/client.routes.js
+++ app/routes/client.routes.js
@@ -4,12 +4,15 @@ module.exports = app => {
     const tokenMiddleware = require('../middleware/token.middleware');
     const identifierMiddleware = require('../middleware/identifier.middleware'); 
     const rdv = require('../controllers/rdv.controller');
+    const horaireController = require('../controllers/horaire.controller');
 
     router.post('/rdv/emp',rdv.getListAvailableEmploye);
     router.post('/rdv/insert',rdv.setRdv);
     router.get('/rdv/list',rdv.getListRDV);
     router.delete('/rdv/delete',rdv.deleteRdv);
     router.get('/rdvs',rdv.getListRDV);
+    
+    router.get('/horaire/list',horaireController.getListHoraire);
     
     app.use('/api/clients', tokenMiddleware.checkTokenExistance,tokenMiddleware.decryptToken,identifierMiddleware.getInfoClient,router);
 }
